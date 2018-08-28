1db62ce29c2a7d4df022b47766d168e7f5ad0f69
informuscle <infomuscle10@gmail.com>
2018-08-28 10:47:39 +0900

    사칙연산

--- 10869.py
+++ 10869.py
@@ -1,3 +1,12 @@
+# 문제
+# 두 자연수 A와 B가 주어진다. 이 때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+# 출력
+# 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
 numIn = input().split()
 print(int(numIn[0]) + int(numIn[1]))
 print(int(numIn[0]) - int(numIn[1]))
