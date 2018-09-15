b94d78a57e93a7da1978a0521d8473146f9a3471
informuscle <infomuscle10@gmail.com>
2018-09-15 22:52:31 +0900

    카카오

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]
+for i in range(len(record)):
+    box = record[i].split()
+    print(box)
+
+# a = "Enter uid muzi"
+#
+# b = a.split(" ")
+# print(b)
