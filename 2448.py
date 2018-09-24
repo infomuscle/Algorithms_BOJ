18197c938c1ccdc9ec278e79af41f9fd82c25a39
infomuscle <infomuscle10@gmail.com>
2018-09-24 19:27:45 +0900

    별찍기 - 11

--- 2448.py
+++ 2448.py
@@ -1,33 +1,32 @@
-# 문제
-# 예제를 보고 별찍는 규칙을 유추한 뒤에 별을 찍어 보세요.
-
-# 입력
-# 첫째 줄에 N이 주어진다. N은 항상 3*2^k 수이다.
-# (3, 6, 12, 24, 48, ...) (k<=10)
-
-# 출력
-# 첫째 줄부터 N번째 줄까지 별을 출력한다.
-
-# n = 3,6,12,24,48
-# 5, 11, 23, 47
-
 import sys
+import math
 
 lines = []
 
-def starPrinter(n, k):
+def starAdder(n, idx):
     stars = ["*", "* *", "*****"]
+    k = round(math.log(n, 2) - math.log(3, 2))
 
-    s = " "*(n-k) + stars[k-1] + " "*(n-k)
-    lines.append(s)
+    if idx == 0:
+        for i in range(3):
+            lines.append(stars[i])
+    else:
+        start = 6*(2**(idx-1))-1
+        for i in range(0, len(lines)):
+            space = " "*(start-2*i)
+            s = lines[i] + space + lines[i]
+            lines.append(s)
 
-    if k == n:
-        for i in range(0, n):
-            print(lines[i])
+    if idx == k:
         return
-    starPrinter(n, k+1)
-
+    starAdder(n, idx+1)
 
 n = int(sys.stdin.readline())
+k = round(math.log(n, 2) - math.log(3, 2))
+length = 6*(2**k)-1
 
-starPrinter(n, 1)
+starAdder(n, 0)
+
+for i in range(0, len(lines)):
+    ll = lines[i].center(length, " ")
+    print(ll)
