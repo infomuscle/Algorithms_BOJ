a4cdb810c057d32a52cb62efad545773046c0d7d
infomuscle <infomuscle10@gmail.com>
2018-09-26 22:47:39 +0900

    에라토스테네스의 체

--- 1929.py
+++ 1929.py
@@ -1,4 +1,29 @@
+# 문제
+# M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1≤M≤N≤1,000,000)
+
+# 출력
+# 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+
 import sys
+import math
 
 m, n = map(int, sys.stdin.readline().split())
+primes = []
 
+for i in range(m, n+1):
+    if i == 1:
+        continue
+    elif i == 2:
+        print(i)
+    else:
+        end = int(math.sqrt(i))+1
+        flag = 1
+        for j in range(2, end+1):
+            if i%j == 0:
+                flag = 0
+                break
+        if flag == 1:
+            print(i)
