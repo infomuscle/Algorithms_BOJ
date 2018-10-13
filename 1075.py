e73f156fb4f276206dd9c32365442c611811321c
Bokeun Aaron Jeong <infomuscle10@gmail.com>
2018-10-13 23:20:14 +0900

    나누기 - 풀이 중

---
+++ 1075.py
@@ -0,0 +1,20 @@
+import sys
+
+n = int(sys.stdin.readline())
+f = int(sys.stdin.readline())
+
+for i in range(2):
+    n /= 10
+n *= 100
+
+while True:
+    if n%f == 0:
+        break
+    else:
+        n += 1
+
+n2 = n%10
+n1 = (n%100)/10
+
+print(n1, end="")
+print(n2)
