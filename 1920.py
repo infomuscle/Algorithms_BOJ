a4513e45ce3f0de2138fb58636c1299eef1f4acf
infomuscle <infomuscle10@gmail.com>
2018-10-22 12:26:02 +0900

    수 찾기 - 풀이 중

--- 1920.py
+++ 1920.py
@@ -9,21 +9,20 @@ finds = list(map(int, sys.stdin.readline().split()))
 nums.sort()
 
 for i in range(m):
-    num = -1
+    num = None
     cnt = 0
-    mid = int(len(nums) / 2)
     left, right = 0, len(nums)-1
+    mid = int(left+right/2)
     while True:
         cnt += 1
+        mid = int((left + right) / 2)
         if finds[i] == nums[mid]:
             num = nums[mid]
             break
         elif finds[i] > nums[mid]:
             left = mid+1
-            mid = int((left+right)/2)
         else:
             right = mid-1
-            mid = int(left+right/2)
         if cnt > math.log(n, 2):
             break
 
