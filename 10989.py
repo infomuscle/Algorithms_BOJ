97d1d652026c9cd02af7ec74dc969c7561e889d9
infomuscle <infomuscle10@gmail.com>
2018-10-24 09:33:32 +0900

    수 정렬하기 3

--- 10989.py
+++ 10989.py
@@ -1,13 +1,24 @@
+# 문제
+# N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다.
+# 둘째 줄부터 N개의 줄에는 숫자가 주어진다.
+# 이 수는 10,000보다 작거나 같은 자연수이다.
+
+# 출력
+# 첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
 import sys
 
 n = int(sys.stdin.readline())
-nums = []
+counts = [0]*10000
 
 for i in range(0, n):
     num = int(sys.stdin.readline())
-    nums.append(num)
+    counts[num-1] += 1
 
-nums.sort()
-
-for i in range(0, n):
-    print(nums[i])
+for i in range(10000):
+    if counts[i] != 0:
+        for j in range(counts[i]):
+            print(i+1)
